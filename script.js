18939fc84a7e7deb182bff6e7a8c5aa8fcadd5bb
kiwijuice56 <alfaroeric127@gmail.com>
2023-04-11 20:17:16 -0500

    Rearranged layout and added text

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ function toggle_popup(id) {
 	document.getElementById("root").classList.toggle("locked");
 }
 
-
 var checkpoint = 0;
 
 window.addEventListener("scroll", function() {
 	var elementTarget = document.getElementById("checkpoint_a");
 	if (checkpoint == 0 && window.scrollY > (elementTarget.offsetTop + elementTarget.offsetHeight)) {
 		toggle_popup("popup_a");
+		checkpoint++;
+	}
+	
+	elementTarget = document.getElementById("checkpoint_b");
+	if (checkpoint == 1 && window.scrollY > (elementTarget.offsetTop + elementTarget.offsetHeight)) {
+		toggle_popup("popup_b");
 		checkpoint++;
 	}
 });
